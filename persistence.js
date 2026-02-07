16e6b6690a8d303fda938b04707741f3f0729e74
Jenny Zebua <jennyzebua@Jennys-MacBook-Air.local>
2026-02-07 20:23:15 +0300

    Adding functions into persistence layer

---
+++ persistence.js
@@ -0,0 +1,34 @@
+const { write } = require('fs')
+const fs = require('fs/promises')
+
+async function readAssignments() {
+    let assignments = await fs.readFile("assignments.json", "utf8")
+    let assignment = JSON.parse(assignments)
+    return assignment
+}
+
+async function readShifts() {
+    let shiftData = await fs.readFile("shifts.json", "utf8")
+    let shifts = JSON.parse(shiftData)
+    return shifts
+}
+
+async function readEmployees() {
+    let data = await fs.readFile("employees.json", "utf8")
+    let employees = JSON.parse(data)
+    return employees
+}
+
+async function saveEmployeeList(employeeInfo) {
+    await fs.writeFile('employees.json', JSON.stringify(employeeInfo, null, 2));
+}
+
+async function saveAssignments(assingmentsInfo) {
+    await fs.writeFile('assingments.json', JSON.stringify(assingmentsInfo, null, 2));
+}
+async function addEmployee(newEmployee) {
+    let employeeList = await readEmployees();
+    employeeList.push(newEmployee)
+    await saveEmployeeList(employeeList);
+}
+module.exports = {readAssignments, readShifts, readEmployees, saveEmployeeList, saveAssignments, addEmployee}
